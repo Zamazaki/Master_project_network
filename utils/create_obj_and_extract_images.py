52768afb017f81576a57b9eea143c9f3ffcad88a
Emma Lu Eikemo <emmalu.eikemo@gmail.com>
2023-11-09 16:41:39 +0100

    Changes script to be compatable with Facegen SDK version 3.V.2

--- utils/create_obj_and_extract_images.py
+++ utils/create_obj_and_extract_images.py
@@ -1,8 +1,6 @@
 import os
 import glob
 
-# To be used inside the Preview folder within the FaceGen SDK
-
 # In order to run this in git bash, first run this line
 # alias python='winpty python.exe'
 # (or try the permanent solution: https://stackoverflow.com/questions/32597209/python-not-working-in-the-command-line-of-git-bash)
@@ -10,6 +8,9 @@ import glob
 input_dir = "Auto_generated_faces/"
 output_dir = "obj_faces/"
 image_dir = "jpg_faces/"
+
+fg3_path = "C:/Users/Public/Fg3Sdk3V2Full/Fg3Sdk3V2Full/bin/win/x64/vs22/release/fg3.exe"
+fgbl_path = "C:/Users/Public/Fg3Sdk3V2Full/Fg3Sdk3V2Full/bin/win/x64/vs22/release/fgbl.exe"
 
 # Delete old output directory
 if(os.path.isdir(output_dir)):
@@ -39,11 +40,12 @@ print(len(files))
 for fil in files:
     basename = os.path.basename(fil)  # Example: "rand0001.fg"
     filename = os.path.splitext(basename)[0] # Example: "rand0001"
-    
+
     # Create .fgmesh file out of the .fg file and place it in the same folder as the .fg file
-    os.system(f"fg3 cssm Face/Face {input_dir}{filename}.fg {input_dir}{filename}.fgmesh")
+    os.system(f"{fg3_path} apply ssm Face/Face {input_dir}{filename}.fg {input_dir}{filename}.fgmesh")
 
     # Create .obj file out of the .fgmesh file and place it in the newly created output file
-    os.system(f"fgbl mesh convert {input_dir}{filename}.fgmesh {output_dir}{filename}.obj")
+    os.system(f"{fgbl_path} mesh convert {input_dir}{filename}.fgmesh {output_dir}{filename}.obj")
     
-
+    # Remove the .fgmesh file after using it, so we save space
+    os.system(f"rm {input_dir}{filename}.fgmesh")
