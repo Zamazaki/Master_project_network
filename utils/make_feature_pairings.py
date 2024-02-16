fa7ddc44daa69f7be66f3030bd06c06c88570ea7
Emma Lu Eikemo <emmalu.eikemo@gmail.com>
2024-02-16 15:43:57 +0100

    Optimized for speed

--- utils/make_feature_pairings.py
+++ utils/make_feature_pairings.py
@@ -1,6 +1,9 @@
 import os
 import numpy as np
 
+# NOTICE: Current system is set up for FaceGen generated face pairs,
+# so a new system will be used for BU3D-FE and BU4D-FE
+
 path_feat_2d = "feature_vectors/2d"
 path_feat_3d = "feature_vectors/2d"
 
@@ -19,25 +22,35 @@ second_quarter_start = quarter_amount
 third_quarter_start = quarter_amount*2
 forth_quarter_start = quarter_amount*3
 
-# TODO: combine the three first for loops 
+
 for i in range(quarter_amount):
+    # Correct pairing female.jpg female.obj
     pairing_array.append(feature_vectors_2d[i])
     pairing_array.append(feature_vectors_3d[i])
     pairing_array.append(str(1))
-
-for i in range(quarter_amount):
+    
+    # Incorrect pairing female.jpg male.obj
     pairing_array.append(feature_vectors_2d[second_quarter_start + i])
     pairing_array.append(feature_vectors_3d[third_quarter_start + i])
     pairing_array.append(str(0))
     
-for i in range(quarter_amount):
+    # Incorrect pairing male.jpg female.obj
     pairing_array.append(feature_vectors_2d[third_quarter_start + i])
     pairing_array.append(feature_vectors_3d[second_quarter_start + i])
     pairing_array.append(str(0))
-
-for i in range(forth_quarter_start, number_of_pairings):
-    pairing_array.append(feature_vectors_2d[i])
-    pairing_array.append(feature_vectors_3d[i])
-    pairing_array.append(str(1))
     
+    # Correct pairing male.jpg male.obj
+    pairing_array.append(feature_vectors_2d[forth_quarter_start + i])
+    pairing_array.append(feature_vectors_3d[forth_quarter_start + i])
+    pairing_array.append(str(1))
+
+# If there are remaining unpaired features, they are matched with each other
+forth_quarter_end = forth_quarter_start + quarter_amount
+if forth_quarter_end < number_of_pairings:
+    for i in range(forth_quarter_end, number_of_pairings):
+        pairing_array.append(feature_vectors_2d[i])
+        pairing_array.append(feature_vectors_3d[i])
+        pairing_array.append(str(1))
+    
+# Save the array as a .csv
 np.savetxt("feature_vectors/feature_pairings.csv", pairing_array, delimiter=",", fmt='%s')
